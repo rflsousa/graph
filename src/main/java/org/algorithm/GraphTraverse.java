2585143486d6d1cc31a851fd8bd6557d08ba8dd0
rflsousa <rafael.oliveira.osv@gmail.com>
2023-04-19 20:57:28 -0300

    BFS Traversal

--- src/main/java/org/algorithm/GraphTraverse.java
+++ src/main/java/org/algorithm/GraphTraverse.java
@@ -3,24 +3,48 @@ package org.algorithm;
 import java.util.ArrayList;
 
 public class GraphTraverse {
-    public static void depthFirstTraversal(Vertex start, ArrayList<Vertex> visitedVertices){
+    public static void depthFirstTraversal(Vertex start, ArrayList<Vertex> visitedVertices) {
         System.out.println(start.getData());
 
-        for (Edge e : start.getEdges()){
+        for (Edge e : start.getEdges()) {
             Vertex neighbor = e.getEnd();
 
-            if(!visitedVertices.contains(neighbor)){
+            if (!visitedVertices.contains(neighbor)) {
                 visitedVertices.add(neighbor);
                 GraphTraverse.depthFirstTraversal(neighbor, visitedVertices);
             }
         }
     }
+
+    public static void breadthFirstSearch(Vertex start, ArrayList<Vertex> visitedVertices) {
+        Queue visitQueue = new Queue();
+        visitQueue.enqueue(start);
+        while (!visitQueue.isEmpty()) {
+            Vertex current = visitQueue.dequeue();
+            System.out.println(current.getData());
+
+            for (Edge e: current.getEdges()) {
+                Vertex neighbor = e.getEnd();
+                if (!visitedVertices.contains(neighbor)) {
+                    visitedVertices.add(neighbor);
+                    visitQueue.enqueue(neighbor);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         TestGraph test = new TestGraph();
         Vertex startingVertex = test.getStartingVertex();
-        ArrayList<Vertex> visitedVertices = new ArrayList<>();
-        visitedVertices.add(startingVertex);
+        ArrayList<Vertex> visitedVertices1 = new ArrayList<>();
+        ArrayList<Vertex> visitedVertices2 = new ArrayList<>();
+        visitedVertices1.add(startingVertex);
+        visitedVertices2.add(startingVertex);
 
-        GraphTraverse.depthFirstTraversal(startingVertex, visitedVertices);
+        System.out.println("DFS");
+        GraphTraverse.depthFirstTraversal(startingVertex, visitedVertices1);
+
+        System.out.println("BFS");
+        GraphTraverse.breadthFirstSearch(startingVertex, visitedVertices2);
     }
 }
