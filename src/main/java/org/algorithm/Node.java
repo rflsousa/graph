d55c103de1636485ea3fde9d3a74fc3a9391c45d
rflsousa <rafael.oliveira.osv@gmail.com>
2023-04-19 20:57:01 -0300

    new data structures

---
+++ src/main/java/org/algorithm/Node.java
@@ -0,0 +1,19 @@
+package org.algorithm;
+
+public class Node {
+    public Vertex data;
+    private Node next;
+
+    public Node(Vertex data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public void setNextNode(Node node) {
+        this.next = node;
+    }
+
+    public Node getNextNode() {
+        return this.next;
+    }
+}
